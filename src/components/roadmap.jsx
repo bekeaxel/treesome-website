f9a8f607353031784569cb0fda1fcc7f7af4b9d3
axel beke <axel@beke.se>
2022-01-16 16:46:08 +0100

    updates to roadmap

--- src/components/roadmap.jsx
+++ src/components/roadmap.jsx
@@ -7,7 +7,51 @@ function Roadmap() {
 
     return (
         <div className="roadmap" id="roadmap">
-            <div className="container">
+
+            <div class="container">
+                <h1 className="header text-t">ROADMAP v1</h1>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="main-timeline">
+                            <div class="timeline">
+                                <a class="timeline-content">
+                                    <div class="timeline-icon"><p class="text-t icon">25%</p></div>
+                                    
+                                    <p class="description text-t">
+                                        25 unique giveaways for holders only
+                                    </p>
+                                </a>
+                            </div>
+                            <div class="timeline">
+                                <a class="timeline-content">
+                                    <div class="timeline-icon"><p class="text-t icon">50%</p></div>
+                                    <p class="description text-t">
+                                        Donation to forest charity
+                                    </p>
+                                </a>
+                            </div>
+                            <div class="timeline">
+                                <a class="timeline-content">
+                                    <div class="timeline-icon"><p class="text-t icon">75%</p></div>
+                                    <p class="description text-t">
+                                        MAYC NFT giveaway + exclusive alpha
+                                    </p>
+                                </a>
+                            </div>
+                            <div class="timeline">
+                                <a class="timeline-content">
+                                    <div class="timeline-icon"><p class="text-t icon">100%</p></div>
+                                    <p class="description text-t">
+                                        Companion collection [free for holders]    
+                                    </p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* <div className="container">
                 <h1 className="header text-t">ROADMAP LAUNCH</h1>
                 <div className="row">
                     <div className="col-2">
@@ -57,7 +101,7 @@ function Roadmap() {
                         <p className="text-t row-text">something is happening</p>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 }
